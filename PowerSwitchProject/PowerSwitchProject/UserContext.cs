8d482e913078d25718cbb1a18bd6a90e4562d6c5
rsx16rus <rsx16rus@gmail.com>
2017-10-06 16:34:31 +0400

    work0

---
+++ PowerSwitchProject/PowerSwitchProject/UserContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace PowerSwitchProject
+{
+    class UserContext:DbContext
+    {
+        public UserContext():base("DbConnection")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Electrical_Substation> Electrical_Substations { get; set; }
+        public DbSet<Group_PS> Group_PSes { get; set; }
+        public DbSet<RES> RESes { get; set; }
+        public DbSet<Switch_model> Switch_models { get; set; }
+        public DbSet<Operating_switch> Operating_switches { get; set; }
+    }
+}
