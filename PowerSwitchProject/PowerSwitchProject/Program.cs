642cf37467202ac81f88fc62737b9d6d4930b30e
rsx16rus <rsx16rus@gmail.com>
2017-10-06 16:05:29 +0400

    +entity framevork

--- PowerSwitchProject/PowerSwitchProject/Program.cs
+++ PowerSwitchProject/PowerSwitchProject/Program.cs
@@ -6,11 +6,7 @@ using System.Windows.Forms;
 
 namespace PowerSwitchProject
 {
-<<<<<<< HEAD
-    // это изменение сделано для merge-тренировки на ветке master
-=======
-    // это изменение сделано для merge-тренировки на ветке merge_train
->>>>>>> merge_train
+
     static class Program
     {
         /// <summary>
